2c1d7ddef085f4077e1b0f5ef1f9b5ed59278c53
Kurov-Oleg <74539138+Kurov-Oleg@users.noreply.github.com>
2021-08-29 22:41:11 +0300

    Update 1.go

--- go/1.go
+++ go/1.go
@@ -1,1 +1,13 @@
+package main
 
+import "fmt"
+
+func main() {
+    fmt.Print("Enter a metr: ")
+    var input float64
+    fmt.Scanf("%f", &input)
+
+    output := input / 0.3048
+
+    fmt.Println("There are ",output, "futs")    
+}
